6379e9563aa35dc8b1613e01bae55ea557a50e91
kulkarnipratham <144027290+kulkarnipratham@users.noreply.github.com>
2024-04-18 08:45:17 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 10 + 10) // we are using nodejs, not browser
+
+console.log(10
+    +
+     10) // code readability should be high
+
+console.log("Pratham")
+
+
+let name = "Pratham"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
